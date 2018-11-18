2fa7ecd88191d4b6254d36e5651fc23c2f167131
Felix A. Gladisch <felix@felixgladisch.de>
2018-11-18 10:26:11 +0100

    step 2 completed

--- seed-restaurants.js
+++ seed-restaurants.js
@@ -6,50 +6,50 @@ const dynamodb = new AWS.DynamoDB.DocumentClient()
 const ssm = new AWS.SSM()
 
 let restaurants = [
-  { 
-    name: "Fangtasia", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png", 
-    themes: ["true blood"] 
+  {
+    name: "Fangtasia",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png",
+    themes: ["true blood"]
   },
-  { 
-    name: "Shoney's", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png", 
-    themes: ["cartoon", "rick and morty"] 
+  {
+    name: "Shoney's",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png",
+    themes: ["cartoon", "rick and morty"]
   },
-  { 
-    name: "Freddy's BBQ Joint", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png", 
-    themes: ["netflix", "house of cards"] 
+  {
+    name: "Freddy's BBQ Joint",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png",
+    themes: ["netflix", "house of cards"]
   },
-  { 
-    name: "Pizza Planet", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png", 
-    themes: ["netflix", "toy story"] 
+  {
+    name: "Pizza Planet",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png",
+    themes: ["netflix", "toy story"]
   },
-  { 
-    name: "Leaky Cauldron", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png", 
-    themes: ["movie", "harry potter"] 
+  {
+    name: "Leaky Cauldron",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png",
+    themes: ["movie", "harry potter"]
   },
-  { 
-    name: "Lil' Bits", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png", 
-    themes: ["cartoon", "rick and morty"] 
+  {
+    name: "Lil' Bits",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png",
+    themes: ["cartoon", "rick and morty"]
   },
-  { 
-    name: "Fancy Eats", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png", 
-    themes: ["cartoon", "rick and morty"] 
+  {
+    name: "Fancy Eats",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png",
+    themes: ["cartoon", "rick and morty"]
   },
-  { 
-    name: "Don Cuco", 
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png", 
-    themes: ["cartoon", "rick and morty"] 
+  {
+    name: "Don Cuco",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png",
+    themes: ["cartoon", "rick and morty"]
   },
 ];
 
 const getTableName = async () => {
-  return `restaurants-${STAGE}-yancui`
+  return `restaurants-${STAGE}-felixgladisch`
 }
 
 const run = async () => {
@@ -62,8 +62,8 @@ const run = async () => {
       Item: x
     }
   }))
-  
-  const req = { 
+
+  const req = {
     RequestItems: {}
   }
   req.RequestItems[tableName] = putReqs
