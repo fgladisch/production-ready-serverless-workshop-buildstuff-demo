b45afc5d9c1a85956059f8b1572479e3730eacb9
Felix A. Gladisch <felix@felixgladisch.de>
2018-11-18 12:14:33 +0200

    step 3 completed

---
+++ tests/steps/init.js
@@ -0,0 +1,20 @@
+let initialized = false;
+
+const init = async () => {
+  if (initialized) {
+    return;
+  }
+
+  process.env.restaurants_api =
+    "https://huw6t9vqjd.execute-api.eu-west-1.amazonaws.com/dev/restaurants";
+  process.env.restaurants_table = "restaurants-dev-felixgladisch";
+	process.env.order_events_stream = "orders-dev-yancui";
+	process.env.restaurant_notification_topic = 'restaurants-dev-yancui'
+  process.env.AWS_REGION = "eu-west-1";
+
+  initialized = true;
+};
+
+module.exports = {
+  init
+};
